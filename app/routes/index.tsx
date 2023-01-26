312529f87e9ed4d859d3f44cb007a9fc0d03c9ee
Yotam Elkaslasy <yotamelkaslasy@gmail.com>
2023-01-26 18:40:13 +0200

    deploy fix

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,7 @@
 export default function Index() {
   return (
-    <div className="flex h-screen items-center justify-center bg-slate-700">
-      <h2 className="text-5xl font-extrabold text-blue-600">
-        TailwindCSS Is Working!
-      </h2>
+    <div className="flex h-screen items-center justify-center bg-slate-100">
+      Yo
     </div>
   );
 }
